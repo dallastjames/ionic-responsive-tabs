8f09100774093100e740884d26d4075cfdd7951f
Dallas James <dallastjames@gmail.com>
2020-12-03 11:18:08 -0700

    init to win it

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  constructor(private actionSheet: ActionSheetController) {}
 
-  constructor() {}
-
+  async more(): Promise<void> {
+    const sheet = await this.actionSheet.create({
+      buttons: [
+        {
+          icon: 'star-outline',
+          text: 'Tab Title 4',
+        },
+        {
+          icon: 'alert-circle-outline',
+          text: 'Tab Title 5',
+        },
+        {
+          icon: 'basketball-outline',
+          text: 'Tab Title 6',
+        },
+      ],
+    });
+    await sheet.present();
+  }
 }
